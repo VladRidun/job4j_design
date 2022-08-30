6092a9a2b56bbccd5405447dd2bdfdb168900bd1
VladRidun <ridunvv@gmail.com>
2022-08-30 10:43:41 +0400

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/collection/tree/SimpleTree.java
+++ src/main/java/ru/job4j/collection/tree/SimpleTree.java
@@ -1,12 +1,41 @@
 package ru.job4j.collection.tree;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
 
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> pred = el -> el.children.size() > 2;
+        return findByPredicate(pred).isEmpty();
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Predicate<Node<E>> pred = el -> el.value.equals(value);
+        return findByPredicate(pred);
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> pred) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (pred.test(el)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
     }
 
     @Override
@@ -17,22 +46,6 @@ public class SimpleTree<E> implements Tree<E> {
         if (elparent.isPresent() && elchild.isEmpty()) {
             elparent.get().children.add(new Node<>(child));
             rsl = true;
-        }
-        return rsl;
-    }
-
-    @Override
-    public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.children);
         }
         return rsl;
     }
