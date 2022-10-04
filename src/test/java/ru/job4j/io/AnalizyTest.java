1c6fe53c3e0ef886542253acfa6fc5f4717f2902
VladRidun <ridunvv@gmail.com>
2022-10-04 10:53:51 +0400

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AnalizyTest {
+    @Test
+    void drop(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("200 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        File target = tempDir.resolve("target.txt").toFile();
+
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(str -> rsl.append(str + System.lineSeparator()));
+        }
+        assertThat(String.format("%s%n%s%n", " 10:58:01; 10:59:01;", " 11:01:02; 11:02:02;"))
+                .isEqualTo(rsl.toString());
+    }
+}
