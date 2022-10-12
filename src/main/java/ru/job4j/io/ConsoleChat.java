85b1f1f77793da4dacc363e00436ebfda71c93bd
VladRidun <ridunvv@gmail.com>
2022-10-12 17:08:04 +0400

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        Scanner input = new Scanner(System.in);
+        List<String> log = new ArrayList<>();
+        System.out.println("Чат открыт");
+        String message = input.nextLine();
+        log.add(message);
+        while (!message.equalsIgnoreCase(OUT)) {
+                message = readPhrases();
+                log.add(message);
+                System.out.println(message);
+                message = input.nextLine();
+                log.add(message);
+            if (message.equals(STOP)) {
+                do {
+                    message = input.nextLine();
+                    log.add(message);
+                } while (!message.equalsIgnoreCase(CONTINUE));
+            }
+        }
+        saveLog(log);
+        input.close();
+    }
+
+    private String readPhrases() {
+        List<String> phrases = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(botAnswers))) {
+            br.lines().forEach(phrases::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return phrases.get(new Random().nextInt(phrases.size()));
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            log.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("./chat/log.txt", "./chat/bot.txt");
+        cc.run();
+    }
+}
