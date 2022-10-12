423feb37bde05fbc7c24e4211f59244ec444e842
VladRidun <ridunvv@gmail.com>
2022-10-12 14:11:30 +0400

    5.2. Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -14,8 +14,8 @@ public class Zip {
     public static void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path path : sources) {
-                zip.putNextEntry(new ZipEntry(path.toAbsolutePath().toString()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toAbsolutePath().toString()))) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toString()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -23,28 +23,22 @@ public class Zip {
             e.printStackTrace();
         }
     }
-
-    public static void validate(String[] args) {
-        if (args.length < 3) {
-            throw new IllegalArgumentException("Use 3 arguments: root folder, file extension");
-        }
-        Path path = Path.of(args[0]);
+    public static void validate(ArgsName args) {
+        Path path = Path.of(args.get("d"));
         if (!Files.exists(path) && !Files.isDirectory(path)) {
             throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER");
         }
-        if (!args[1].startsWith(".")) {
-            throw new IllegalArgumentException("Specify the file extension with point");
+        if (!args.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Specify the file extension");
         }
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Use 3 arguments: root folder, file extension");
+        }
         ArgsName argsName = ArgsName.of(args);
-        String[] arrayArgs = {
-                argsName.get("d"),
-                argsName.get("e"),
-                argsName.get("o")
-        };
-        validate(arrayArgs);
+        validate(argsName);
         List<Path> sources = search(Path.of(argsName.get("d")), p -> !p.toFile().getName().endsWith(argsName.get("e")));
         packFiles(sources, new File(argsName.get("o")));
     }
