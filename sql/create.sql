a71fa2aa08a597189defd61d1d2d8ede2849a6e7
VladRidun <ridunvv@gmail.com>
2022-11-21 13:53:07 +0400

    1. Схема прав пользователи и роли.111

--- sql/create.sql
+++ sql/create.sql
@@ -1,24 +1,34 @@
+create table roles(
+     id serial primary key,
+     role_name varchar(255)
+);
+
 create table users (
     id serial primary key,
     name varchar(255),
     roles_id int references roles(id),
 );
-
- create table roles(
-     id serial primary key,
-     role_name varchar(255)
- );
  
  create table rules(
      id serial primary key,
      rule_name varchar(255)
- );
+);
  
  create table roles_rules(
      id serial primary key,
      roles_id int references roles(id),
      rules_id int references rules(id)
- );
+);
+
+ create table category(
+     id serial primary key,
+     category_name varchar(255)
+);
+
+ create table state(
+     id serial primary key,
+     state_name varchar(255)
+);
 
  create table item(
      id serial primary key,
@@ -28,24 +38,14 @@ create table users (
      state_id int references state(id)
  );
 
- create table category(
-     id serial primary key,
-     category_name varchar(255),
- );
-
- create table state(
-     id serial primary key,
-     state_name varchar(255),
- );
-
- create table comments(
+create table comments(
      id serial primary key,
      comment text,
      item_id int references item(id)
- );
+);
 
- create table attachs(
+create table attachs(
      id serial primary key,
      attach text,
      item_id int references item(id)
- );
+);
--- sql/insert.sql
+++ sql/insert.sql
@@ -3,7 +3,7 @@ insert into roles(name) values ('user');
 insert into rules(name) values ('read, write, edit');
 insert into roles_rules (roles_id, rules_id) values (1, 1);
 
-insert into employees(name, roles_id) VALUES ('Vlad', 1);
+insert into users(name, roles_id) VALUES ('Vlad', 1);
 
 insert into category(category_name) values ('SAP');
 insert into state(state_name) values ('Подана');
