5ae20f4e19f1f69ade9c1bfab78ef3d47cf48636
VladRidun <ridunvv@gmail.com>
2022-11-22 11:15:48 +0400

    1. Схема прав пользователи и роли.

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,6 +1,5 @@
-
-insert into roles(name) values ('user');
-insert into rules(name) values ('read, write, edit');
+insert into roles(role_name) values ('user');
+insert into rules(rule_name) values ('read, write, edit');
 insert into roles_rules (roles_id, rules_id) values (1, 1);
 
 insert into users(name, roles_id) VALUES ('Vlad', 1);
@@ -10,5 +9,5 @@ insert into state(state_name) values ('Подана');
 
 insert into item(item_number, users_id, category_id, state_id) VALUES ('00001', 1, 1, 1 );
 
-insert into comments(comment, item(id)) values ('Не выгружается отчет', 1);
-insert into attachs(attach, item(id)) values ('C:\Users\OVROQ2R\Desktop\скрин_ошибки.jpg', 1);
+insert into comments(comment, item_id) values ('Не выгружается отчет', 1);
+insert into attachs(attach, item_id) values ('C:\Users\OVROQ2R\Desktop\скрин_ошибки.jpg', 1);
