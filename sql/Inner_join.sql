bbe9b1bd3cf5be9a30d2b723f0b044316a25a2e0
VladRidun <ridunvv@gmail.com>
2022-11-23 06:36:26 +0400

    1. Inner join

--- sql/Inner_join.sql
+++ sql/Inner_join.sql
@@ -1,3 +1,24 @@
+create table phonenumber(
+    id serial primary key,
+    number int
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255),
+    phonenumber_id int references phonenumber(id) unique
+);
+
+insert into phonenumber(number) values (67434);
+insert into phonenumber(number) values (64532);
+insert into phonenumber(number) values (67424);
+
+insert into people(name,phonenumber_id) values ('Borka', 1);
+insert into people(name,phonenumber_id) values ('Masha', 2);
+insert into people(name,phonenumber_id) values ('Natasha', 3);
+insert into people(name) values ('Oleg');
+insert into people(name) values ('Vika');
+
 select pp.name as Имя,  pn.number as Номер
 from people as pp join phonenumber as pn on pp.phonenumber_id = pn.id;
 
