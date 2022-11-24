df9187724db0a3b36c224de888056590996a84cb
VladRidun <ridunvv@gmail.com>
2022-11-24 15:03:21 +0400

    1. Inner join 2

--- sql/Inner_join.sql
+++ sql/Inner_join.sql
@@ -1,29 +1,25 @@
-create table phonenumber(
-    id serial primary key,
-    number int
-);
-
 create table people(
     id serial primary key,
-    name varchar(255),
-    phonenumber_id int references phonenumber(id) unique
+    name varchar(255)
 );
 
-insert into phonenumber(number) values (67434);
-insert into phonenumber(number) values (64532);
-insert into phonenumber(number) values (67424);
+create table phonenumber(
+    id serial primary key,
+    number int,
+	people_id int references people(id) unique
+);
 
-insert into people(name,phonenumber_id) values ('Borka', 1);
-insert into people(name,phonenumber_id) values ('Masha', 2);
-insert into people(name,phonenumber_id) values ('Natasha', 3);
-insert into people(name) values ('Oleg');
-insert into people(name) values ('Vika');
+insert into people(name) values ('Borka'), ('Masha'), ('Natasha'), ('Oleg'), ('Vika');
+
+insert into phonenumber(number, people_id) values (67434, 1);
+insert into phonenumber(number, people_id) values (64532, 2);
+insert into phonenumber(number, people_id) values (67424, 3);
 
 select pp.name as Имя,  pn.number as Номер
-from people as pp join phonenumber as pn on pp.phonenumber_id = pn.id;
+from people as pp join phonenumber as pn on pn.people_id = pp.id;
 
 select  pn.number as "Номер телефона"
-from people as pp join phonenumber as pn on pp.phonenumber_id = pn.id;
+from people as pp join phonenumber as pn on pn.people_id = pp.id;
 
 select pp.name as "Имя человека"
-from people as pp join phonenumber as pn on pp.phonenumber_id = pn.id;
+from people as pp join phonenumber as pn on pn.people_id = pp.id;
