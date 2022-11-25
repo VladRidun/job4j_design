b7c39777a5037eef794c162fce3e277a7a6da6b0
VladRidun <ridunvv@gmail.com>
2022-11-25 16:17:51 +0400

    3. Фильтры [#1732]

---
+++ sql/filters.sql
@@ -0,0 +1,78 @@
+create table type(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table product(
+    id serial primary key,
+    name varchar(255),
+    type_id int references type(id), 
+    expired_date timestamp, 
+    price float
+);
+
+insert into type(name) values ('Сыр');
+insert into type(name) values ('Молоко');
+insert into type(name) values ('Мороженое');
+
+
+insert into product(name, type_id, expired_date, price) values('сыр Пармезман', 1, date '2022-10-01', 654);
+insert into product(name, type_id, expired_date, price) values('сыр Мараздам', 1, date '2022-12-12', 705);
+insert into product(name, type_id, expired_date, price) values('сыр Сфета', 1, date '2022-11-25', 850);
+insert into product(name, type_id, expired_date, price) values('сыр брОнза', 1, date '2022-12-26', 560);
+
+insert into product(name, type_id, expired_date, price) values('Молоко Село Зеленое реально прям зеленое', 2, date '2022-12-12', 134);
+insert into product(name, type_id, expired_date, price) values('Молоко Домик в какой-то там деревне', 2, date '2022-11-23', 152);
+insert into product(name, type_id, expired_date, price) values('Молоко С Котом в тельянежке', 2, date '2022-07-06', 106);
+insert into product(name, type_id, expired_date, price) values('Молоко от Молочника под кайфом', 2, date '2022-07-13', 104);
+
+insert into product(name, type_id, expired_date, price) values('Мороженое шоколадное мое любимое', 3, date '2022-08-15', 56);
+insert into product(name, type_id, expired_date, price) values('Мороженое Пломбик', 3, date '2022-09-12', 65);
+insert into product(name, type_id, expired_date, price) values('Мороженое Ванилька', 3, date '2022-03-09', 78);
+insert into product(name, type_id, expired_date, price) values('Мороженое Фисташечка', 3, date '2022-05-09', 98);
+
+select * from product
+
+select p.name as "Название продукта", p.expired_date as "Тип продукта", p.price as "Цена"
+from product as p where type_id = 1;
+
+select p.name as "Название продукта", p.expired_date as "Тип продукта", p.price as "Цена"
+from product as p where p.name LIKE '%Мороженое%';
+
+select p.name as "Название продукта", p.expired_date as "Тип продукта", p.price as "Цена"
+from product as p where p.expired_date  < '01.11.2022';
+
+select p.name as "Название продукта", p.price as "Цена"
+from product as p order by p.price desc
+limit 1;
+
+select t.name as "Тип продукта", count(p.id)
+from product as p 
+join type as t 
+on p.type_id = t.id
+group by t.name;
+
+select p.name as "Название продукта", t.name as "Тип продукта", p.price as "Цена"
+from product as p 
+join type as t 
+on p.type_id = t.id
+where t.name like '%Сыр%' or t.name like '%Молоко%';
+
+select t.name as "Тип продукта", count(p.id)
+from product as p 
+join type as t 
+on p.type_id = t.id
+group by t.name
+having t.name like '%Сыр%' and count(p.id) < 10;
+
+select p.name as "Название продукта", t.name as "Тип продукта"
+from product as p 
+join type as t 
+on p.type_id = t.id;
+
+
+
+
+
+
+
